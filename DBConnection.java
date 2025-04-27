8d9a2d54b6fe9aa5d4b4bdfd6c8062e291251ee1
Sodkhuu1 <bsodkhuu0604@gmail.com>
2025-04-27 21:54:09 +0800

    Database tei holboh class

---
+++ DBConnection.java
@@ -0,0 +1,18 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    // Өгөгдлийн сантай холбогдох функц
+    public static Connection getConnection() throws SQLException {
+        // Хост, хэрэглэгчийн нэр, нууц үгээ тохируулна
+        String url = "jdbc:mysql://localhost:3306/online_hospital";
+        String username = "root";
+        String password = "password";
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
